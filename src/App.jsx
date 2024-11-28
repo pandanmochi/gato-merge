04dccd84c2589caf8822727a46c4d113af6e36bd
pandanmochi <to.un.thi@gmail.com>
2024-11-28 11:08:20 +0100

    feat: add handlers for drops in the work bench area and try merging cats

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,84 @@
-import { WorkBench } from './components/WorkBench';
 import { CatMenu } from './components/CatMenu';
+import { WorkBench } from './components/WorkBench';
 import { useCatData } from './hooks/useCatData';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const App = () => {
-    const { initialCats, allCats, combinations } = useCatData();
-
-    const [workBenchCats, setWorkBenchCats] = useState();
+    const { initialCats, combinations } = useCatData();
     const [unlockedCats, setUnlockedCats] = useState(initialCats);
+    const [workBenchItems, setWorkBenchItems] = useState([]);
+    const workBenchRef = useRef(null);
 
     /**
-     * Attempts to merge two cats.
+     * Handles the drop of an item over the work bench area.
+     * @param {Object} eventData
+     * @param {string} eventData.name
+     * @param {string} eventData.x
+     * @param {string} eventData.y
+     */
+    const handleItemDrop = ({ name, x, y }) => {
+        const workbench = workBenchRef.current.children[0];
+        const workbenchChildren = Array.from(workbench.children);
+        const dropTargetIndex = workbenchChildren.findIndex((child) => {
+            console.log(child);
+            const rect = child.getBoundingClientRect();
+            return (
+                x >= rect.left &&
+                x <= rect.right &&
+                y >= rect.top &&
+                y <= rect.bottom
+            );
+        });
+
+        if (
+            dropTargetIndex !== -1 &&
+            workBenchItems[dropTargetIndex] !== name
+        ) {
+            mergeCats(workBenchItems[dropTargetIndex], name, dropTargetIndex);
+        } else {
+            setWorkBenchItems((prevWorkBenchItems) => [
+                ...prevWorkBenchItems,
+                name,
+            ]);
+        }
+    };
+
+    /**
+     * Attempts to merge two cats into one.
      * @param {string} catA
      * @param {string} catB
+     * @param {number} workBenchPosition
      */
-    const mergeCats = (catA, catB) => {};
-
-    const handleMerge = (catA, catB) => {};
+    const mergeCats = (catA, catB, workBenchPosition) => {
+        const comboKey = [catA, catB].sort().join(', ');
+        const comboVal = combinations[comboKey];
+        if (typeof comboVal === 'string') {
+            setUnlockedCats((prevUnlockedCats) => {
+                if (!prevUnlockedCats.includes(comboVal)) {
+                    return [...prevUnlockedCats, comboVal];
+                }
+                return prevUnlockedCats;
+            });
+            setWorkBenchItems((prevWorkBenchItems) => {
+                const newWorkBenchItems = [...prevWorkBenchItems];
+                newWorkBenchItems[workBenchPosition] = comboVal;
+                return newWorkBenchItems;
+            });
+        } else {
+            setWorkBenchItems((prevWorkBenchItems) => [
+                ...prevWorkBenchItems,
+                catB,
+            ]);
+        }
+    };
 
     return (
         <div className="flex h-screen w-screen flex-col bg-orange-950 text-white">
             <div className="flex h-full w-full">
-                <div className="w-3/4">
+                <div ref={workBenchRef} className="w-3/4">
                     <WorkBench
-                        workBenchCats={workBenchCats}
-                        onMerge={handleMerge}
+                        items={workBenchItems}
+                        onItemDrop={handleItemDrop}
                     />
                 </div>
                 <div className="w-1/4">
