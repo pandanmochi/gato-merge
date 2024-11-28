6edd853b5ed0105e81bf6dd90d563303d77f68ba
pandanmochi <to.un.thi@gmail.com>
2024-11-28 11:05:32 +0100

    feat(components): add listeners for drag starts and handle accordingly

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,32 @@
-import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
 
-/**
- * Item consisting of cat image and label.
- * @param {Object} props
- * @param {string} props.name
- * @param {boolean} props.isCatMenu
- */
 export const Item = ({ name, isCatMenu = false }) => {
+    const itemRef = useRef(null);
+
+    useEffect(() => {
+        const handleDragStart = (event) => {
+            // Attach item name to dataTransfer for later identification.
+            event.dataTransfer.setData('text/plain', name);
+        };
+
+        // Listen to drag starts.
+        const refCurrent = itemRef.current;
+        refCurrent.addEventListener('dragstart', handleDragStart);
+
+        // Remove the listener when unmounting the component.
+        return () => {
+            refCurrent.removeEventListener('dragstart', handleDragStart);
+        };
+    }, [name]);
+
     return (
         <div className="flex h-32 flex-row items-center space-x-3">
             <img
                 src={`/src/assets/png/${name}.png`}
-                className={clsx('h-32 w-32 border-4 border-yellow-500')}
-                alt={`Cat ${name}`}
+                className="h-32 w-32 border-4 border-yellow-500"
+                alt={name}
+                draggable={true}
+                ref={itemRef}
             />
             {isCatMenu && <span className="text-center">{name}</span>}
         </div>
