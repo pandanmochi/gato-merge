8787d1476fc2da9063a71bd90627833766fc7424
pandanmochi <to.un.thi@gmail.com>
2024-11-28 11:07:32 +0100

    feat(components): add listeners for drops in work bench and pass on to app

--- src/components/WorkBench.jsx
+++ src/components/WorkBench.jsx
@@ -1,15 +1,33 @@
 import { Item } from './Item';
 
-/**
- * Item grid for displaying a number of items in a grid.
- * @param {string[]} workBenchCats
- * @param {() => void} onMerge Function to be called when two cats are merging.
- */
-export const WorkBench = ({ workBenchCats, onMerge }) => {
+export const WorkBench = ({ items, onItemDrop }) => {
+    /**
+     * Handles drop events triggered by items dropped over the working area.
+     */
+    const handleDrop = (event) => {
+        event.preventDefault();
+        const name = event.dataTransfer.getData('text/plain');
+        const { clientX: x, clientY: y } = event;
+
+        // If there is a name associated with the drop then pass the relevant information to the app.
+        if (name) {
+            onItemDrop({ name, x, y });
+        }
+    };
+
     return (
-        <div className="grid h-screen grid-flow-col grid-cols-5 space-x-2 p-7">
-            {workBenchCats?.map((cat, index) => (
-                <Item key={`item-${index}`} name={cat} />
+        <div
+            id="drop-zone"
+            className="grid h-screen grid-flow-col grid-cols-5 grid-rows-4 items-center py-7 pl-12"
+            onDrop={handleDrop}
+            onDragOver={(e) => e.preventDefault()}
+        >
+            {items.map((item, key) => (
+                <Item
+                    key={`workbench-item-${key}`}
+                    isCatMenu={false}
+                    name={item}
+                />
             ))}
         </div>
     );
