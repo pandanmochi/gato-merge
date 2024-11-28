06026c200b5a763932db42572b1698d83663c2b2
pandanmochi <to.un.thi@gmail.com>
2024-11-28 05:40:36 +0100

    feat(components): add first cat menu implementation

--- src/components/CatMenu.jsx
+++ src/components/CatMenu.jsx
@@ -1,3 +1,11 @@
-export const CatMenu = () => {
-	return <div className="cat-menu"></div>;
+import { Item } from './Item';
+
+export const CatMenu = ({ selectableCats }) => {
+    return (
+        <div className="h-screen flex-row space-y-2 overflow-y-scroll border-l-2 border-yellow-400 bg-green-900 p-3">
+            {selectableCats.map((cats, index) => (
+                <Item key={`item-${index}`} name={cats} isCatMenu={true} />
+            ))}
+        </div>
+    );
 };
