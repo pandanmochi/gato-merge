a84b3cfd9b5fcda60fbc0ad1e090cd378a8d12ea
pandanmochi <to.un.thi@gmail.com>
2024-11-28 05:40:24 +0100

    feat(components): add first work bench implementation

--- src/components/WorkBench.jsx
+++ src/components/WorkBench.jsx
@@ -1,3 +1,16 @@
-export const WorkBench = () => {
-	return <div>Hey</div>;
+import { Item } from './Item';
+
+/**
+ * Item grid for displaying a number of items in a grid.
+ * @param {string[]} workBenchCats
+ * @param {() => void} onMerge Function to be called when two cats are merging.
+ */
+export const WorkBench = ({ workBenchCats, onMerge }) => {
+    return (
+        <div className="grid h-screen grid-flow-col grid-cols-5 space-x-2 p-7">
+            {workBenchCats?.map((cat, index) => (
+                <Item key={`item-${index}`} name={cat} />
+            ))}
+        </div>
+    );
 };
