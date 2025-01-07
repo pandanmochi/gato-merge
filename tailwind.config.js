b323f548022cda11cdc1ff6750867932aee6b0d9
pandanmochi <to.un.thi@gmail.com>
2025-01-07 16:28:53 +0100

    add color palette to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+            colors: {
+                'gato-blue-1': '#EEFEFF',
+                'gato-blue-2': '#3275DA',
+                'gato-blue-3': '#12305E',
+                'gato-light-yellow': '#FFFEF1',
+                'gato-violet': '#B7BDFF',
+                'gato-pink-1': '#FBF3FE',
+                'gato-pink-2': '#FFB7E2',
+            }
+        },
 	},
 	plugins: [],
 };
