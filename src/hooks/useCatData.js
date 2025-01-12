405f1097ad28f103b8b6a89b3e5bba39f38a7f4a
pandanmochi <to.un.thi@gmail.com>
2025-01-12 02:40:16 +0100

    add more combinations

--- src/hooks/useCatData.js
+++ src/hooks/useCatData.js
@@ -3,15 +3,29 @@
  * @returns Initial cats, all unlockable cats and their combinations.
  */
 export const useCatData = () => {
-    const initialCats = ['el-gato', 'happy'];
-    const allCats = ['el-gato', 'happy', 'standing'];
+    const initialCats = ['el-gato', 'maxwell', 'hehe', 'crying'];
+    
     const combinations = {
         'el-gato, happy': 'standing',
+        'el-gato, maxwell': 'spoingus',
+        'el-gato, hehe': 'happy',
+        'el-gato, grumpy': 'sus',
+        'huh, maxwell': 'dissociated',
+        'hehe, maxwell': 'melvin',
+        'spoingus, sus': 'vibing',
+        'banana, crying': 'banana-crying',
+        'crying, hehe': 'crying-thumbs',
+        'olli, sus': 'huh',
+        'smudge, vibing': 'apple',
+        'apple, bop': 'apple-cat',
+        'bop, olli': 'biting',
+        'crying, melvin': 'super-crying',
+        'nyan, standing': 'smurf',
+        'milk-face, standing': 'full-of-soup',
     };
 
     return {
         initialCats,
-        allCats,
         combinations,
     };
 };
