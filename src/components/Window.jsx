079e8a4a3dc6ebfb006d8406b9d24a446d2fa6d6
pandanmochi <to.un.thi@gmail.com>
2025-01-12 17:27:27 +0100

    Window: remove defaultProps

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -2,11 +2,17 @@ import PropTypes from 'prop-types';
 
 export const Window = ({ children, windowTitle, tabColor, bgColor }) => {
     return (
-        <div className="shadow-gato rounded-lg border-2 border-gato-blue-2">
-            <div className={`rounded-t-md border-b-2 border-gato-blue-2 px-3 py-2 font-semibold ${ tabColor }`}>
-                { windowTitle }
+        <div className="rounded-lg border-2 border-gato-blue-2 shadow-gato">
+            <div
+                className={`rounded-t-md border-b-2 border-gato-blue-2 px-3 py-2 font-semibold ${tabColor ? tabColor : 'bg-gato-violet'}`}
+            >
+                {windowTitle}
             </div>
-            <div className={`rounded-b-md ${ bgColor }`}>{children}</div>
+            <div
+                className={`rounded-b-md ${bgColor ? bgColor : 'bg-gato-light-yellow'}`}
+            >
+                {children}
+            </div>
         </div>
     );
 };
@@ -16,9 +22,4 @@ Window.propTypes = {
     windowTitle: PropTypes.string,
     tabColor: PropTypes.string,
     bgColor: PropTypes.string,
-};
-
-Window.defaultProps = {
-    tabColor: 'bg-gato-violet',
-    bgColor: 'bg-gato-light-yellow',
 };
