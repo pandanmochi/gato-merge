20c2227241c16e892a9befe807ef762dbba721c6
pandanmochi <to.un.thi@gmail.com>
2025-01-16 23:48:58 +0100

    add new combinations and unlockables

--- src/hooks/useCatData.js
+++ src/hooks/useCatData.js
@@ -3,36 +3,63 @@
  * @returns Initial cats, all unlockable cats and their combinations.
  */
 export const useCatData = () => {
-    const initialCats = ['el-gato', 'maxwell', 'hehe', 'crying'];
-    
-    const combinations = {
-        'el-gato, happy': 'standing',
-        'el-gato, maxwell': 'spoingus',
-        'el-gato, hehe': 'happy',
-        'el-gato, grumpy': 'sus',
-        'huh, maxwell': 'dissociated',
-        'hehe, maxwell': 'melvin',
-        'spoingus, sus': 'vibing',
-        'banana, crying': 'banana-crying',
-        'crying, hehe': 'crying-thumbs',
-        'olli, sus': 'huh',
-        'smudge, vibing': 'apple',
-        'apple, bop': 'apple-cat',
-        'bop, olli': 'biting',
-        'crying, melvin': 'super-crying',
-        'nyan, standing': 'smurf',
-        'milk-face, standing': 'full-of-soup',
-    };
+  const initialCats = ['el-gato', 'maxwell', 'hehe', 'crying'];
 
-    // Cats that are unlocked after a combination is made.
-    const unlockable = {
-        'happy': 'grumpy',
+  const combinations = {
+    'el-gato, happy': 'standing',
+    'el-gato, maxwell': 'spoingus',
+    'el-gato, hehe': 'happy',
+    'el-gato, grumpy': 'sus',
+    'huh, maxwell': 'dissociated',
+    'hehe, maxwell': 'melvin',
+    'spoingus, sus': 'vibing',
+    'banana, crying': 'banana-crying',
+    'crying, hehe': 'crying-thumbs',
+    'olli, sus': 'huh',
+    'smudge, vibing': 'apple',
+    'apple, bop': 'apple-cat',
+    'bop, olli': 'biting',
+    'crying, melvin': 'super-crying',
+    'crying, grumpy': 'smudge',
+    'nyan, standing': 'smurf',
+    'bop, crying': 'milk-face',
+    'milk-face, standing': 'full-of-soup',
+  };
 
-    }
+  // Cats that are unlocked after a combination is made.
+  const unlockable = {
+    happy: 'grumpy',
+    apple: 'banana',
+    melvin: 'olli',
+    smudge: 'nyan',
+    smurf: 'bop',
+  };
 
-    return {
-        initialCats,
-        combinations,
-        unlockable
-    };
+  // const catText = {
+  //     'el-gato': 'standing',
+  //     'standing': '',
+  //     'maxwell': 'spoingus',
+  //     'hehe': 'happy',
+  //     'grumpy': 'sus',
+  //     'huh': 'dissociated',
+  //     'melvin': 'melvin',
+  //     'spoingus': 'vibing',
+  //     'banana': 'banana-crying',
+  //     'crying': 'crying-thumbs',
+  //     'sus': 'huh',
+  //     'vibing': 'apple',
+  //     'apple': 'apple-cat',
+  //     'bop': 'biting',
+  //     'olli': 'super-crying',
+  //     'nyan': 'smurf',
+  //     'smurf': '',
+  //     'milk-face': 'full-of-soup',
+  //     'full-of-soup': ''
+  // }
+
+  return {
+    initialCats,
+    combinations,
+    unlockable,
+  };
 };
